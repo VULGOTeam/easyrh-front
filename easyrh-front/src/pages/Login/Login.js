9356340d706d53917bda97189f7b1a09bf0275b7
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2021-05-27 10:31:33 -0300

    Tela login

--- easyrh-front/src/pages/Login/Login.js
+++ easyrh-front/src/pages/Login/Login.js
@@ -5,38 +5,29 @@ import './Login.css';
 
 function Login(){
     return (
-            <Container>
-               <Row>
-                 <Col class="col-md-12">
-                        <Form class="form-login .form-group">
-                            <Row>
-                                <Form.Label>EasyRH</Form.Label>
-                            </Row>
-                            <Row>
-                                <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>E-MAIL</Form.Label>
-                                    <Form.Control type="email" />
-                                    <Form.Text className="text-muted">
-                                    Utilize seu e-mail cadastrado para receber o código de acesso
-                                    ou digite o seu melhor e-mail para criar uma nova conta
-                                    </Form.Text>
-                                </Form.Group>
-                            </Row>
-                            <Row>
-                                <Button variant="primary" type="submit">
-                                Continuar
-                                </Button>
-                            </Row>
-                            <Row>
-                            <Form.Text className="text-muted">
-                            Se ainda não possuir uma conta, fique tranquilo que criaremos uma para você
-                            </Form.Text>
-                            </Row>
-                            
-                    </Form>
-                </Col>
-                </Row>
-        </Container>
+    <div class="box">
+        <div class="logo-header">
+            <h1>EasyRH </h1>
+        </div>
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label><p class="font-weight-bold">E-MAIL</p></Form.Label>
+                <Form.Control type="email" placeholder="" />
+                
+                <Form.Text className="text-muted">
+                Utilize seu e-mail cadastrado para receber o código de acesso
+                ou digite o seu melhor e-mail para criar uma nova conta
+                </Form.Text>
+            </Form.Group>
+            <Row>
+                <div class="col-12 mt-5">
+                    <Button variant="primary" type="submit" className="button-primary">
+                        Continuar
+                    </Button>
+                </div>
+            </Row>
+        </Form>
+    </div>
     );
 }
 
