215f0ca67c743434c2248fdfa1f133645565bece
Ariane Barros <arianepaulabarros@gmail.com>
2021-05-28 23:05:35 -0300

    Dashboard e Imagem de fundo

--- easyrh-front/src/pages/Dashboard/Dashboard.js
+++ easyrh-front/src/pages/Dashboard/Dashboard.js
@@ -1,65 +1,62 @@
-import {Container, Table} from 'react-bootstrap';
+import {Container, Table, Row, Col, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
 
 function Dashboard(){
 
-    const func = ["Mark", "Financeiro" , "2.000"]
+    const func = ["Mark", "Financeiro"]
 
    
     return(
-        <Container>
-            <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th colSpan="2">Nome</th>
-                <th colSpan="3">Setor</th>
-                <th colSpan="4">Salário</th>
-                <th colSpan="5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                    </svg>
-                </th>
-                </tr>
-            </thead>
-            <tbody>
-                
-                <tr>
-                <td>1</td>
-                <td colSpan="2">{func[0]}</td>
-                <td colSpan="3">{func[1]}</td>
-                <td colSpan="4">{func[2]}</td>
-                <td colSpan="5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                </td>
-                </tr>
-                <tr>
-                <td>2</td>
-                <td colSpan="2">{func[0]}</td>
-                <td colSpan="3">{func[1]}</td>
-                <td colSpan="4">{func[2]}</td>
-                <td colSpan="5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                    </svg>
-                </td>
-                </tr>
-                <tr>
-                <td>3</td>
-                <td colSpan="2">{func[0]}</td>
-                <td colSpan="3">{func[1]}</td>
-                <td colSpan="4">{func[2]}</td>
-                <td colSpan="5">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                </svg>
-                </td>
-                </tr>
-            </tbody>
-            </Table>
+        <Container fluid>
+           
+            <Row>
+                <Col class="col-12 mt-4 mb-2 pl-5">
+                    <h1>EasyRH</h1>
+                    <p>NOME EMPRESA LOGADA</p>
+                    <p>CNPJ - CNPJ AQUI</p>
+                    <Button variant="secundary">Sair</Button>
+                </Col>
+            </Row>
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <Table striped bordered hover responsive>                
+                            <thead>
+                                <tr>
+                                <th className="text-center">#</th>
+                                <th colSpan="2">Nome</th>
+                                <th colSpan="3">Setor</th>
+                                <th colSpan="4" className="text-center">
+                                    
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                    </svg>
+                                </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Array.from({ length: 8 }).map((_, index) => (
+                                    <tr>
+                                
+                                    <td className="text-center">{index+1}</td>
+                                    <td colSpan="2">{func[0]}</td>
+                                    <td colSpan="3">{func[1]}</td>
+                                    <td className="text-center" colSpan="4">
+                                        <Button  variant="secundary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                            </svg>
+                                        </Button>
+                                    </td>
+                                    </tr>
+                                ))}
+                            </tbody>                
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     )
 }
